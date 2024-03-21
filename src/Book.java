44cebd255ab382b0cc0c95ee5fc3a0d6d1149aa9
Kaspis <kasparas.ziuraitis@gmail.com>
2024-03-21 15:32:59 +0200

    get-set prie BOOKS

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,27 @@
 public class Book {
+    private String title;
+    private int pages;
+    private int releaseYear;
+    public void setTitle(String name) {
+        this.title = name;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public Book(){
+
+    }
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+    public int getPages(){
+        return this.pages;
+    }
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+    public int getReleaseYear(){
+        return this.releaseYear;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Book knyga = new Book ();
+        knyga.setTitle("Nesbo");
+        knyga.setPages(600);
+        knyga.setReleaseYear(1990);
+//        System.out.println(knyga.getTitle());
+//        System.out.println(knyga.getPages());
+//        System.out.println(knyga.getReleaseYear());
+
+        Book knyga1 =new Book();
+        knyga1.setTitle("Lietuvos istorija");
+        knyga1.setPages(650);
+        knyga1.setReleaseYear(1998);
+//        System.out.println(knyga1.getTitle());
+//        System.out.println(knyga1.getPages());
+
+
+        Book knyga2 = new Book();
+        knyga2.setTitle("20.000 myliu po vandeniu");
+        knyga2.setPages(350);
+        knyga2.setReleaseYear(1998);
+
+
+
+
+
+
+
+
+
     }
 
 
